6864bc8bac097c0885893cd597dc505676d837a8
Mintzr95 <m88240996@gmail.com>
2026-09-16 21:24:10 +0300

    Expand project settings into a real engine project model

--- mju/project/project.h
+++ mju/project/project.h
@@ -1,7 +1,20 @@
 #pragma once
+#include <cstdint>
 #include <string>
+
 namespace mju::project {
-struct Settings { std::string name="MJU Game"; int width=1280; int height=720; int target_fps=60; bool portrait=true; };
+struct Settings {
+    std::string name="MJU Game";
+    std::string start_scene="main.mjuscene";
+    int width=1280;
+    int height=720;
+    int target_fps=60;
+    bool portrait=true;
+    bool vsync=true;
+    int max_entities=65536;
+    std::string renderer="gles2";
+};
+
 bool save_settings(const Settings&, const std::string& path);
 bool load_settings(Settings&, const std::string& path);
 }
