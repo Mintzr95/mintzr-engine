eeb6b517d0a130d2e113ca737111c56f24c692f6
Mintzr95 <m88240996@gmail.com>
2026-09-16 18:47:25 +0300

    Integrate MJU sprite batching into GLES2 renderer

--- mju/render/renderer_gles2.h
+++ mju/render/renderer_gles2.h
@@ -2,6 +2,7 @@
 #include <GLES2/gl2.h>
 #include <cstddef>
 #include "../core/scene.h"
+#include "sprite_batch.h"
 
 namespace mju {
 
@@ -16,6 +17,7 @@ public:
 
     std::size_t last_quad_count() const { return last_quad_count_; }
     std::size_t last_vertex_count() const { return last_vertex_count_; }
+    std::size_t last_batch_count() const { return last_batch_count_; }
 
 private:
     bool check_shader(GLuint shader, const char* stage);
@@ -23,10 +25,14 @@ private:
     GLuint program_ = 0;
     GLint pos_ = -1;
     GLint color_ = -1;
+    GLuint vertex_buffer_ = 0;
+    GLuint index_buffer_ = 0;
     int width_ = 1;
     int height_ = 1;
     std::size_t last_quad_count_ = 0;
     std::size_t last_vertex_count_ = 0;
+    std::size_t last_batch_count_ = 0;
+    render::SpriteBatch batch_{2048};
 };
 
 }
