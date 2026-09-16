9c6196564c414f7c6e2e340df58096e3469b9452
Mintzr95 <m88240996@gmail.com>
2026-09-16 21:44:26 +0300

    Expose scene manager from engine runtime

--- mju/core/engine.h
+++ mju/core/engine.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "scene.h"
+#include "scene_manager.h"
 #include "camera.h"
 #include "input.h"
 #include "animation.h"
@@ -12,22 +13,66 @@
 #include "../scripting/script.h"
 #include "../diagnostics/debug.h"
 #include "../project/project.h"
+
 namespace mju {
+
 class Engine {
 public:
- bool initialize(int w,int h); void resize(int w,int h); void update(float dt); void shutdown();
- Scene& scene(){return scene_;} const Scene& scene()const{return scene_;}
- Camera2D& camera(){return camera_;} const Camera2D& camera()const{return camera_;}
- InputState& input(){return input_;} const InputState& input()const{return input_;}
- AnimationLibrary& animations(){return animations_;} const AnimationLibrary& animations()const{return animations_;}
- AudioSystem& audio(){return audio_;} physics::World& physics(){return physics_;} particles::Emitter& particles(){return particles_;} resources::ResourceCatalog& resources(){return resources_;}
- tilemap::TileMap& tilemap(){return tilemap_;} const tilemap::TileMap& tilemap()const{return tilemap_;}
- ui::Canvas& ui(){return ui_;} const ui::Canvas& ui()const{return ui_;} scripting::ScriptVM& scripts(){return scripts_;} debug::Console& console(){return console_;}
- project::Settings& settings(){return settings_;} const project::Settings& settings()const{return settings_;}
- int width()const{return width_;} int height()const{return height_;} bool initialized()const{return initialized_;} unsigned long long frame_count()const{return frame_count_;}
+    bool initialize(int w, int h);
+    void resize(int w, int h);
+    void update(float dt);
+    void shutdown();
+
+    Scene& scene() { return scene_; }
+    const Scene& scene() const { return scene_; }
+    SceneManager& scene_manager() { return scene_manager_; }
+    const SceneManager& scene_manager() const { return scene_manager_; }
+    Camera2D& camera() { return camera_; }
+    const Camera2D& camera() const { return camera_; }
+    InputState& input() { return input_; }
+    const InputState& input() const { return input_; }
+    AnimationLibrary& animations() { return animations_; }
+    const AnimationLibrary& animations() const { return animations_; }
+    AudioSystem& audio() { return audio_; }
+    physics::World& physics() { return physics_; }
+    particles::Emitter& particles() { return particles_; }
+    resources::ResourceCatalog& resources() { return resources_; }
+    tilemap::TileMap& tilemap() { return tilemap_; }
+    const tilemap::TileMap& tilemap() const { return tilemap_; }
+    ui::Canvas& ui() { return ui_; }
+    const ui::Canvas& ui() const { return ui_; }
+    scripting::ScriptVM& scripts() { return scripts_; }
+    debug::Console& console() { return console_; }
+    project::Settings& settings() { return settings_; }
+    const project::Settings& settings() const { return settings_; }
+
+    int width() const { return width_; }
+    int height() const { return height_; }
+    bool initialized() const { return initialized_; }
+    unsigned long long frame_count() const { return frame_count_; }
+
 private:
- int width_=0,height_=0; bool initialized_=false; unsigned long long frame_count_=0;
- EntityId demo_entity_=0,button_entity_=0;
- Scene scene_; Camera2D camera_; InputState input_; AnimationLibrary animations_; AudioSystem audio_; physics::World physics_; particles::Emitter particles_; resources::ResourceCatalog resources_; tilemap::TileMap tilemap_; ui::Canvas ui_; scripting::ScriptVM scripts_; debug::Console console_; project::Settings settings_;
+    int width_ = 0;
+    int height_ = 0;
+    bool initialized_ = false;
+    unsigned long long frame_count_ = 0;
+    EntityId demo_entity_ = 0;
+    EntityId button_entity_ = 0;
+
+    Scene scene_;
+    SceneManager scene_manager_;
+    Camera2D camera_;
+    InputState input_;
+    AnimationLibrary animations_;
+    AudioSystem audio_;
+    physics::World physics_;
+    particles::Emitter particles_;
+    resources::ResourceCatalog resources_;
+    tilemap::TileMap tilemap_;
+    ui::Canvas ui_;
+    scripting::ScriptVM scripts_;
+    debug::Console console_;
+    project::Settings settings_;
 };
+
 }
