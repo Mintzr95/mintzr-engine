eb9abb6b2e44a9e3350c24477c8bbafc3f473e0d
Mintzr95 <m88240996@gmail.com>
2026-09-16 19:14:07 +0300

    Expose TileMap and animation library through Engine API

--- mju/core/engine.h
+++ mju/core/engine.h
@@ -7,6 +7,7 @@
 #include "../physics/physics.h"
 #include "../particles/particles.h"
 #include "../resources/resources.h"
+#include "../tilemap/tilemap.h"
 #include "../ui/ui.h"
 #include "../scripting/script.h"
 #include "../diagnostics/debug.h"
@@ -18,16 +19,13 @@ public:
  Scene& scene(){return scene_;} const Scene& scene()const{return scene_;}
  Camera2D& camera(){return camera_;} const Camera2D& camera()const{return camera_;}
  InputState& input(){return input_;} const InputState& input()const{return input_;}
- AudioSystem& audio(){return audio_;}
- physics::World& physics(){return physics_;}
- particles::Emitter& particles(){return particles_;}
- resources::ResourceCatalog& resources(){return resources_;}
- ui::Canvas& ui(){return ui_;} const ui::Canvas& ui()const{return ui_;}
- scripting::ScriptVM& scripts(){return scripts_;}
- debug::Console& console(){return console_;}
+ AnimationLibrary& animations(){return animations_;} const AnimationLibrary& animations()const{return animations_;}
+ AudioSystem& audio(){return audio_;} physics::World& physics(){return physics_;} particles::Emitter& particles(){return particles_;} resources::ResourceCatalog& resources(){return resources_;}
+ tilemap::TileMap& tilemap(){return tilemap_;} const tilemap::TileMap& tilemap()const{return tilemap_;}
+ ui::Canvas& ui(){return ui_;} const ui::Canvas& ui()const{return ui_;} scripting::ScriptVM& scripts(){return scripts_;} debug::Console& console(){return console_;}
  project::Settings& settings(){return settings_;} const project::Settings& settings()const{return settings_;}
  int width()const{return width_;} int height()const{return height_;} bool initialized()const{return initialized_;} unsigned long long frame_count()const{return frame_count_;}
 private:
- int width_=0,height_=0; bool initialized_=false; unsigned long long frame_count_=0; Scene scene_; Camera2D camera_; InputState input_; AudioSystem audio_; physics::World physics_; particles::Emitter particles_; resources::ResourceCatalog resources_; ui::Canvas ui_; scripting::ScriptVM scripts_; debug::Console console_; project::Settings settings_;
+ int width_=0,height_=0; bool initialized_=false; unsigned long long frame_count_=0; Scene scene_; Camera2D camera_; InputState input_; AnimationLibrary animations_; AudioSystem audio_; physics::World physics_; particles::Emitter particles_; resources::ResourceCatalog resources_; tilemap::TileMap tilemap_; ui::Canvas ui_; scripting::ScriptVM scripts_; debug::Console console_; project::Settings settings_;
 };
 }
