cc1d09c14de17fb4f3dcedd51d2b3a43bf071065
Mintzr95 <m88240996@gmail.com>
2026-09-16 21:51:49 +0300

    Add unified scene and tilemap render pass

--- mju/render/renderer_gles2.h
+++ mju/render/renderer_gles2.h
@@ -23,6 +23,7 @@ public:
     void draw(const Scene& scene);
     void draw(const Scene& scene, const Camera2D& camera);
     void draw_tilemap(const tilemap::TileMap& map, const Camera2D& camera);
+    void draw_scene_and_tilemap(const Scene& scene, const tilemap::TileMap& map, const Camera2D& camera);
     void end();
     void shutdown();
 
@@ -54,6 +55,8 @@ private:
     void trim_gpu_textures(std::size_t incoming_bytes = 0);
     void destroy_textures();
     void upload_and_draw_batches();
+    void append_scene(const Scene& scene, const Camera2D& camera);
+    void append_tilemap(const tilemap::TileMap& map, const Camera2D& camera);
 
     GLuint program_ = 0;
     GLint pos_ = -1;
