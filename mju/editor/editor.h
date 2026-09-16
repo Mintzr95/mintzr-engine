84e52adb2a991aa41a9adc3ef4eea27616b4d56c
Mintzr95 <m88240996@gmail.com>
2026-09-16 21:56:16 +0300

    Add editor transform transactions for mobile drags

--- mju/editor/editor.h
+++ mju/editor/editor.h
@@ -21,20 +21,20 @@ class EditorState {
 public:
     Scene scene;
     ProjectSettings project;
-    Tool tool=Tool::Select;
-    EntityId selected=0;
-    bool playing=false;
+    Tool tool = Tool::Select;
+    EntityId selected = 0;
+    bool playing = false;
     History history;
     assets::AssetManager assets;
     tilemap::TileMap tilemap;
-    bool grid_enabled=true;
-    float grid_size=32.0f;
-    float zoom=1.0f;
-    bool snap_to_grid=true;
-    void set_zoom(float z){ zoom = z<0.25f?0.25f:(z>4.0f?4.0f:z); }
+    bool grid_enabled = true;
+    float grid_size = 32.0f;
+    float zoom = 1.0f;
+    bool snap_to_grid = true;
+    void set_zoom(float z) { zoom = z < 0.25f ? 0.25f : (z > 4.0f ? 4.0f : z); }
     void snap_position(Entity& e);
 
-    Entity* create(const std::string& name, EntityId parent=0);
+    Entity* create(const std::string& name, EntityId parent = 0);
     bool select(EntityId id);
     Entity* selected_entity();
     bool delete_selected();
@@ -46,6 +46,12 @@ public:
     bool undo();
     bool redo();
     void checkpoint();
+    void begin_transform();
+    void end_transform();
+    bool transform_editing() const { return transform_editing_; }
+
+private:
+    bool transform_editing_ = false;
 };
 
 bool save_project(const ProjectSettings& settings, const std::string& path);
