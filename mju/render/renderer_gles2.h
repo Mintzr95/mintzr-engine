380dd350148c02f6fe22aaff2a6ef5d384662bbd
Mintzr95 <m88240996@gmail.com>
2026-09-16 18:06:45 +0300

    Introduce batched mobile 2D renderer state

--- mju/render/renderer_gles2.h
+++ mju/render/renderer_gles2.h
@@ -1,4 +1,32 @@
 #pragma once
 #include <GLES2/gl2.h>
+#include <cstddef>
 #include "../core/scene.h"
-namespace mju { class GLES2Renderer { public: bool initialize(); void resize(int w,int h); void begin(); void draw(const Scene& scene); void end(); void shutdown(); private: GLuint program_=0; GLint pos_= -1, color_=-1; int width_=1,height_=1; }; }
+
+namespace mju {
+
+class GLES2Renderer {
+public:
+    bool initialize();
+    void resize(int w, int h);
+    void begin();
+    void draw(const Scene& scene);
+    void end();
+    void shutdown();
+
+    std::size_t last_quad_count() const { return last_quad_count_; }
+    std::size_t last_vertex_count() const { return last_vertex_count_; }
+
+private:
+    bool check_shader(GLuint shader, const char* stage);
+    bool check_program(GLuint program);
+    GLuint program_ = 0;
+    GLint pos_ = -1;
+    GLint color_ = -1;
+    int width_ = 1;
+    int height_ = 1;
+    std::size_t last_quad_count_ = 0;
+    std::size_t last_vertex_count_ = 0;
+};
+
+}
