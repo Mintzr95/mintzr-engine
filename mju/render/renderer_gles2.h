c345fff44fa1607c2f400b3a21077c091b9c9827
Mintzr95 <m88240996@gmail.com>
2026-09-16 19:11:06 +0300

    Add GLES2 texture and UV pipeline with cache ownership

--- mju/render/renderer_gles2.h
+++ mju/render/renderer_gles2.h
@@ -1,40 +1,39 @@
 #pragma once
 #include <GLES2/gl2.h>
 #include <cstddef>
+#include <cstdint>
+#include <string>
+#include <unordered_map>
 #include "../core/scene.h"
 #include "../core/camera.h"
+#include "../assets/texture_cache.h"
 #include "sprite_batch.h"
 
 namespace mju {
-
 class GLES2Renderer {
 public:
     bool initialize();
-    void resize(int w, int h);
+    void resize(int w,int h);
     void begin();
     void draw(const Scene& scene);
-    void draw(const Scene& scene, const Camera2D& camera);
+    void draw(const Scene& scene,const Camera2D& camera);
     void end();
     void shutdown();
-
-    std::size_t last_quad_count() const { return last_quad_count_; }
-    std::size_t last_vertex_count() const { return last_vertex_count_; }
-    std::size_t last_batch_count() const { return last_batch_count_; }
-
+    std::size_t last_quad_count() const{return last_quad_count_;}
+    std::size_t last_vertex_count() const{return last_vertex_count_;}
+    std::size_t last_batch_count() const{return last_batch_count_;}
+    std::size_t loaded_texture_count() const{return gpu_textures_.size();}
 private:
-    bool check_shader(GLuint shader, const char* stage);
+    bool check_shader(GLuint shader,const char* stage);
     bool check_program(GLuint program);
-    GLuint program_ = 0;
-    GLint pos_ = -1;
-    GLint color_ = -1;
-    GLuint vertex_buffer_ = 0;
-    GLuint index_buffer_ = 0;
-    int width_ = 1;
-    int height_ = 1;
-    std::size_t last_quad_count_ = 0;
-    std::size_t last_vertex_count_ = 0;
-    std::size_t last_batch_count_ = 0;
+    GLuint texture_for(const std::string& path);
+    void destroy_textures();
+    GLuint program_=0; GLint pos_=-1,color_=-1,uv_=-1,texture_sampler_=-1;
+    GLuint vertex_buffer_=0,index_buffer_=0,white_texture_=0;
+    int width_=1,height_=1;
+    std::size_t last_quad_count_=0,last_vertex_count_=0,last_batch_count_=0;
     render::SpriteBatch batch_{2048};
+    assets::TextureCache texture_cache_;
+    std::unordered_map<std::string,GLuint> gpu_textures_;
 };
-
 }
